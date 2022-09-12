98323e82bd6d0bbbdea23c5b0e6359ee3ae01408
dimam <dimamusorin542@gmail.com>
2022-09-12 19:48:08 +0300

    Выполненная лаб1

---
+++ src/ru/mirea/lab1/Ex5/Example.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab1.Ex5;
+
+public class Example {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
---
+++ src/ru/mirea/lab1/Ex6/Example.java
@@ -0,0 +1,10 @@
+package ru.mirea.lab1.Ex6;
+
+public class Example {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(1. / i);
+        }
+
+    }
+}
