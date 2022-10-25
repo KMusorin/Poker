1fd6ffdb4091d5b515cded50a960584f7ccf58d9
dimam <dimamusorin542@gmail.com>
2022-10-25 21:21:10 +0300

    version2

--- src/ru/mirea/lab1/Ex6/Example.java
+++ src/ru/mirea/lab1/Ex6/Example.java
@@ -3,7 +3,8 @@ package ru.mirea.lab1.Ex6;
 public class Example {
     public static void main(String[] args) {
         for (int i = 1; i < 11; i++) {
-            System.out.println(1. / i);
+//            System.out.println(1. / i);
+            System.out.printf("%.2f\n", 1. / i);
         }
 
     }
