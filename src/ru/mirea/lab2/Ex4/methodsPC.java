0f44cd0dfc67daf0040151b8fdc0935a59d24030
kocmyc <k.musorin@gmail.com>
2023-12-05 12:18:01 +0300

    Delete useless exept Poker

--- src/ru/mirea/lab2/Ex4/methodsPC.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.lab2.Ex4;
-
-public interface methodsPC {
-    void addPC();
-
-    void deletePC();
-
-    void searchPC();
-}
--- src/ru/mirea/lab6/Ex3/Nameable.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.lab6.Ex3;
-
-public interface Nameable {
-
-    String getName();
-
-
-}
